6297a52bb0989bb4d2c89bbe3fdfdf7a6c752111
CVargas2170 <vargascarlos2170@gmail.com>
2025-02-13 00:18:39 -0400

    cambio 2

--- example.py
+++ example.py
@@ -535,4 +535,4 @@ def graph_sales_by_branch(df):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.envigron.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
